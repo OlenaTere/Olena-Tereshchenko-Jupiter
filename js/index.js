2d82de26d79b3bd6d07e4c4f1228a399b101526e
Olena Tereshchenko <olenatereshchenko123@gmail.com>
2025-01-22 17:36:22 -0500

    form and functionality second update

--- js/index.js
+++ js/index.js
@@ -51,32 +51,33 @@ console.log(messageList);
 
 //callback for submit
 //Add an event listener
-messageForm.addEventListener("submit", (event) => {
+messageForm.addEventListener("submit", function (event) {
     //prevent the default refreshing behavior
     event.preventDefault();
     const userName = event.target.usersName.value;
     const email = event.target.usersEmail.value;
     const usersMessage = event.target.usersMessage.value;
-    console.log(userName);
-    console.log(email);
-    console.log(usersMessage);
+    // console.log(userName);
+    // console.log(email);
+    // console.log(usersMessage);
 
-//Create a variable named newMessage
-const newMessage = document.createElement("li");
+    //Create new message list item
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href="mailto:${email}">${userName}</a><span>${usersMessage}</span>`;
 
-//set the inner HTML
-newString = `<a href="mailto:${email}">${userName}</a><span>${usersMessage}</span>`;
-console.log(newString);
-newMessage.innerHTML = newString;
+    //Create and append the removeButton
+    let removeButton = createRemoveButton();
+    newMessage.appendChild(removeButton);
 
-//Append the removeButton to the newMessage element
-newMessage.appendChild(onRemoveButton);
 
-//Append the newMessage to the messageList element
-messageList[0].appendChild(newMessage);
+    //show the message section if it was hidden
+    messageSection.hidden = false;
 
-//reset
-event.target.reset;
+    //Append the newMessage to the list
+    messageList[0].appendChild(newMessage);
+
+    //reset the form
+    event.target.reset();
 
 });
 
@@ -84,14 +85,16 @@ event.target.reset;
 //Add remove button
 //callback for remove button
 //Add an event listener to the removeButton
-function onRemoveButton(event) {
-    const removeButton = document.createElement("button");
+function createRemoveButton() {
+    let removeButton = document.createElement("button");
     removeButton.innerText = "Remove";
     removeButton.setAttribute("type", "button");
-    removeButton.addEventListener("click", () => {
-        const entry = event.target.parentNode;
+    removeButton.addEventListener("click", function() {
+        const entry = removeButton.parentNode;
         entry.remove();
-        if (messageList.childElementCount === 0) {
+        
+        // Hide the message section if there are no messages left
+        if (messageList.children.length === 0) {
             messageSection.hidden = true;
         };
 
@@ -99,23 +102,3 @@ function onRemoveButton(event) {
     return removeButton;
     
 };
-
-
-// //callback for submit
-// //Add an event listener
-// messageForm.addEventListener(`submit`, onSubmitButton);
-// function onSubmitButton(event) {
-//     //prevent the default refreshing behavior
-//     event.preventDefault();
-//     const userName = event.target.usersName.value;
-//     const email = event.target.usersEmail.value;
-//     const usersMessage = event.target.usersMessage.value;
-//     console.log(userName);
-//     console.log(email);
-//     console.log(usersMessage);
-
-// };
-    
-    
-    
-
